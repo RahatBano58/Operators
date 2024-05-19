9cb57fde029a45b6cf684c33c230d131559f5cc1
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-19 21:16:37 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,71 @@
+    //Operators
+
+let a: number =  5;
+let b: number = a+2
+console.log(b)             
+
+let num1: number =10
+let num2: number = 20
+let result = num1 + num2
+console.log(result)       
+
+
+let num3: number =40 
+let num4: number =50
+let result2 = num3 - num4
+console.log(result2)          
+
+
+let num5 =0
+console.log(num5++)     
+console.log(num5)       
+console.log(++num5)     
+
+let num6 =5
+let num7 =4
+console.log(num6 == num7)
+console.log(num6 != num7)
+
+let num8 = 5
+let num9 = 2
+console.log(num8 == num9)     
+console.log(num8 != num9)      
+console.log(5 < 2)            
+console.log(5 > 2)            
+console.log(5 !> 2)            
+console.log(num8++)            
+console.log(num8)            
+console.log(++num9)
+console.log(++num9)
+
+
+let num10 = 9
+let num11 = 8
+console.log(num10 != num11)
+console.log(num10 == num11)
+console.log(num11++)
+console.log(++num10)
+
+
+let c:number = 5;
+let d:number = 2;
+let e:number;
+e = ++c + c++ + --d + d-- + c + d++ + d;
+console.log(e)
+
+
+let f:number = 0
+let g:number = 5
+let h
+h = f++ + f++ + ++g + f++ + f-- + ++g
+console.log(h)
+
+let aa:Number = 2;
+let bb:Number = 4;
+let cc:Number = 5;
+let dd:Number = 8;
+let ee:Number = 4;
+let ff
+ff = (2 + 4 * 5 - 8 / 4);
+console.log(ff);
+
